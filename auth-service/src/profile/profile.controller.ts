839dd9d772d9d7880bcaf201f5c699dd8d1cfcfa
Sheren Aura Vi <sherenaura21@gmail.com>
2026-06-08 14:48:56 +0700

    fix(profile): restrict profiles endpoint to customer only

--- auth-service/src/profile/profile.controller.ts
+++ auth-service/src/profile/profile.controller.ts
@@ -1,15 +1,19 @@
 import { Controller, Get, Request, UseGuards } from '@nestjs/common';
-import { ApiBearerAuth, ApiOperation, ApiTags } from '@nestjs/swagger';
+import { ApiBearerAuth, ApiExcludeController, ApiTags } from '@nestjs/swagger';
 import { JwtAuthGuard } from '../auth/guards/jwt.auth.guard';
+import { RolesGuard } from '../auth/guards/roles.guard';
+import { Roles } from '../auth/decorators/roles.decorator';
 import { ProfileService } from './profile.service';
 
 @ApiTags('Profile')
+@ApiExcludeController()
 @Controller('profiles')
+@UseGuards(JwtAuthGuard, RolesGuard)
+@Roles('CUSTOMER')
 export class ProfileController {
   constructor(private readonly profileService: ProfileService) {}
 
   @Get()
-  @UseGuards(JwtAuthGuard)
   @ApiBearerAuth()
   getProfile(@Request() req) {
     return this.profileService.getProfile(req.user.id);
